fa0e4bbcb59c67df7f471e8a846e7840125660cd
amilin <moh.amilin@moladin.com>
2022-07-29 17:28:08 +0700

    fixing height header

--- components/moleculs/UI/global-header.jsx
+++ components/moleculs/UI/global-header.jsx
@@ -15,11 +15,11 @@ export default function GlobalHeader() {
     leftHeader = 'sd'
   }
   return (
-    <header className="flex items-center justify-center fixed w-full h-32 z-40 bg-white ">
+    <header className="flex items-center justify-center fixed w-full h-30 z-40 bg-white ">
       <Container className="flex items-center justify-between rounded-b-3xl h-full pl-4 bg-primary">
         <Link href="/ass">
           <a className="align-middle">
-            <Images src="/logonav.png" width={70} height={70} />
+            <Images src="/logonav.png" width={45} height={45} />
           </a>
         </Link>
         <div className="-mr-4">
