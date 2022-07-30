ce0c58ba206f17627a4a9e5085bdfd8fedbdfe63
amilin <moh.amilin@moladin.com>
2022-07-30 15:53:34 +0700

    add font roboto

--- components/moleculs/UI/container-category.jsx
+++ components/moleculs/UI/container-category.jsx
@@ -4,7 +4,7 @@ export default function ContainerCategory(props) {
     const {name} = props;
   return (
     <div className="rounded-lg py-2 bg-secondary mt-5 mx-3">
-        <h2 className="px-3 text-lg font-bold text-primary">{name}</h2>
+        <h3 className="px-3 text-sm font-roboto font-bold text-primary">{name}</h3>
     </div>
   )
 }
