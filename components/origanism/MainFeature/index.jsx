5b7bd6c025f47818d03727c0ccc4e08734f1e1ec
amilin <moh.amilin@moladin.com>
2022-07-30 16:52:44 +0700

    add Mainfeature

--- components/origanism/MainFeature/index.jsx
+++ components/origanism/MainFeature/index.jsx
@@ -1,154 +1,119 @@
-import Icon1 from '../../../public/MainIcon/icon1.png'
-import Icon2 from '../../../public/MainIcon/icon2.png'
-import Icon3 from '../../../public/MainIcon/icon3.png'
-import Icon4 from '../../../public/MainIcon/icon4.png'
-import Icon5 from '../../../public/MainIcon/icon5.png'
-import Icon6 from '../../../public/MainIcon/icon6.png'
-import Icon7 from '../../../public/MainIcon/icon7.png'
-import Icon8 from '../../../public/MainIcon/icon8.png'
-import Icon9 from '../../../public/MainIcon/icon9.png'
-import Icon10 from '../../../public/MainIcon/icon10.png'
+/* eslint-disable @next/next/no-img-element */
+// import Icon1 from "../../../public/MainIcon/icon1.png";
+// import Icon2 from "../../../public/MainIcon/icon2.png";
+// import Icon3 from "../../../public/MainIcon/icon3.png";
+// import Icon4 from "../../../public/MainIcon/icon4.png";
+// import Icon5 from "../../../public/MainIcon/icon5.png";
+// import Icon6 from "../../../public/MainIcon/icon6.png";
+// import Icon7 from "../../../public/MainIcon/icon7.png";
+// import Icon8 from "../../../public/MainIcon/icon8.png";
+// import Icon9 from "../../../public/MainIcon/icon9.png";
+// import Icon10 from "../../../public/MainIcon/icon10.png";
 
+// import Image from "next/image";
+import React from "react";
+import { Container } from "../../moleculs/UI/container";
+import ContainerCategory from "../../moleculs/UI/container-category";
 
-import Image from 'next/image'
-import React from 'react'
-import { Container } from '../../moleculs/UI/container'
-import ContainerCategory from '../../moleculs/UI/container-category'
+const people = [
+  {
+    name: "Santri Hub",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon1.png",
+  },
+  {
+    name: "Info Kampus",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon2.png",
+  },
+  {
+    name: "Jalur Masuk",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon3.png",
+  },
+  {
+    name: "Beasiswa",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon5.png",
+  },
+  {
+    name: "Event",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon6.png",
+  },
+  {
+    name: "Info Santri",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon7.png",
+  },
+  {
+    name: "Donasi",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon8.png",
+  },
+  {
+    name: "Artikel",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon9.png",
+  },
+  {
+    name: "E-Courses",
+    role: "Co-Founder / CTO",
+    imageUrl: '../../../MainIcon/icon4.png',
+  },
+  
+  {
+    name: "Komunitas",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "../../../MainIcon/icon10.png",
+  },
+];
 
 export default function MainFeature() {
-    return (
-     <section>
-        <Container>
+  return (
+    <section>
+      <Container>
         <div>
-          <ContainerCategory name={'Kategori'}/>
-
-            {/* <section className="bg-white">
-                <div className="container px-6  mx-auto">
-
-                    <div className="grid grid-cols-2 gap-2 mt-8 xl:mt-2 xl:gap-5  md:grid-cols-5 xl:grid-cols-5">
-
-                        <div className="flex flex-col items-center p-6 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon1}
-                                    height={25}
-                                    width={21}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">Tentang SantriHub</h2>
-
+          <ContainerCategory name={"Kategori"} />
+          <div className="bg-white mt-4" >
+            <div className="max-w-7xl mx-auto px-4 text-center sm:px-6 lg:px-8 lg:py-2">
+              <div className="space-y-8 sm:space-y-12">
+                <ul
+                  role="list"
+                  className="rounded-ful mx-auto grid grid-cols-4 gap-x-4 gap-y-8 sm:grid-cols-5 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-5"
+                >
+                  {people.map((person) => (
+                    <li key={person.name}>
+                      <div className="space-y-4">
+                        <img
+                          className="mx-auto h-18 w-18 rounded-full"
+                          src={person.imageUrl}
+                          alt="Icon feature"
+                        />
+                        <div className="space-y-2">
+                          <div className="text-xs font-medium lg:text-sm">
+                            <h3>{person.name}</h3>
+                          </div>
                         </div>
-
-                        <div className="flex flex-col items-center p-6 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon2}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize  ">Info Kampus</h2>
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-6 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon3}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize  ">Jalur Masuk</h2>
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-6 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon4}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize  ">Beasiswa</h2>
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-6 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon5}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">Pesantren</h2>
-
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-1 space-y-3 text-center">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon6}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">Blog</h2>
-
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-1 space-y-3 text-center">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon7}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">Event</h2>
-
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-1 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon8}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">Kalender</h2>
-
-
-                        </div>
-
-                        <div className="flex flex-col items-center p-1 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon9}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">E-courses</h2>
-
-
-                        </div>
-                        <div className="flex flex-col items-center p-1 space-y-3 text-center ">
-                            <span className="inline-block p-6 text-blue-500 bg-blue-100 rounded-full ">
-                                <Image
-                                    src={Icon10}
-                                />
-                            </span>
-
-                            <h2 className=" font-semibold text-gray-700 capitalize ">Organisasi santri</h2>
-
-
-                        </div>
-
-                    </div>
-                </div>
-            </section > */}
-        </div >
-        </Container>
-     </section>
-    )
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+         
+        </div>
+      </Container>
+    </section>
+  );
 }
