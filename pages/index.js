d465f180fafb2d43e81b6eab15fde64295d961f5
amilin <moh.amilin@moladin.com>
2022-07-30 15:30:26 +0700

    add Mainfeature

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { HomePageContent } from '../components/moleculs/home/homepage-content'
 import { Page } from '../components/moleculs/page'
 import { Container } from '../components/moleculs/UI/container'
+import MainFeature from '../components/origanism/MainFeature'
 import MainHero from '../components/origanism/MainHero'
 import Blog1 from '../public/MainHero/1.jpg'
 
@@ -13,6 +14,7 @@ export default function Home() {
     <HomePageContent>
     <Container className="space-y-3 mt-3">
     <MainHero/>
+    <MainFeature/>
       <div className='mt-20 mb-10 m-3'>
       <p>
       lorem ipsun
