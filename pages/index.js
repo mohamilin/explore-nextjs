135dd4e86aef6f725322777477d6e1e52d7541b0
amilin <moh.amilin@moladin.com>
2022-07-30 14:03:37 +0700

    add MainHero

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Head from 'next/head'
 import Image from 'next/image'
 import { HomePageContent } from '../components/moleculs/home/homepage-content'
 import { Page } from '../components/moleculs/page'
 import { Container } from '../components/moleculs/UI/container'
+import MainHero from '../components/origanism/MainHero'
+import Blog1 from '../public/MainHero/1.jpg'
 
 export default function Home() {
   return (
   <Page>
     <HomePageContent>
-    <Container className="space-y-3">
+    <Container className="space-y-3 mt-10">
+    <MainHero/>
       <div className='mt-20 mb-10 m-3'>
       <p>
       lorem ipsun
